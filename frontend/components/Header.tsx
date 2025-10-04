4a7218870d7fa2df2143ba77af83ffd2c8fb38c6
tai-shis <tshis070@mtroyal.ca>
2025-10-04 13:44:51 -0600

    kitty can see colours and has found no page

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className='bg-global p-6'>
+      <h1 className='text-center font-extrabold text-3xl font-mojo'>MoJo</h1>
+    </header> 
+  )
+}
+
+export default Header
---
+++ frontend/pages/NoPage.tsx
@@ -0,0 +1,14 @@
+function NoPage() {
+  return(
+    <>
+      <div className='border-2 rounded-xl bg-body border-body my-16 mx-4 p-4 text-center shadow-lg shadow-black'>
+        <p className='text-xl font-mojo'>
+          404 No Page Found.<br/>
+          Please report this issue to a developer (us)
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default NoPage;
