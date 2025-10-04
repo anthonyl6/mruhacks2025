2663949b2dc8cbcc1b73eba7f13c9ad2ace7af26
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-10-04 16:23:59 -0600

    Refactor ScreenContent and TabGroup components to enforce type safety for selected tabs and improve styling of input elements

--- mojo/components/ScreenContent.tsx
+++ mojo/components/ScreenContent.tsx
@@ -3,14 +3,14 @@ import { Platform, Pressable, Text, TextInput, View } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { TabGroup } from './TabGroup';
-import { CameraIcon, LinkIcon } from 'lucide-react-native';
+import { LinkIcon } from 'lucide-react-native';
 
 export const ScreenContent = () => {
   const isAndroid15 = Platform.OS === 'android' && Platform.Version >= 35;
-  const [selectedTab, setSelectedTab] = useState('home');
+  const [selectedTab, setSelectedTab] = useState<'send' | 'request'>('send');
   const tabs = [
-    { label: 'Send', value: 'send' },
-    { label: 'Request', value: 'request' },
+    { label: 'Send', value: 'send' as const },
+    { label: 'Request', value: 'request' as const },
   ];
   const WrapperComponent = isAndroid15 ? View : SafeAreaView;
   return (
@@ -21,11 +21,12 @@ export const ScreenContent = () => {
           <TabGroup selectedTab={selectedTab} onSelect={setSelectedTab} tabs={tabs} />
           <View className="mx-12 flex flex-row items-center justify-center gap-2">
             <TextInput
-              className="placeholder:text-foreground flex-1 items-center justify-center overflow-hidden rounded-md bg-[#212121] p-3"
-              placeholder="Search"
+              inputMode="email"
+              className="placeholder:text-foreground/50 text-foreground flex-1 items-center justify-center overflow-hidden rounded-full bg-[#212121] p-6 px-6"
+              placeholder="email@example.com"
             />
             <Pressable
-              className="items-center justify-center overflow-hidden rounded-md bg-[#212121] p-2"
+              className="items-center justify-center overflow-hidden rounded-full bg-[#212121] p-5 transition-all duration-300 active:bg-[#3f3f3f]"
               onPress={() => {
                 console.log('search');
               }}>
--- mojo/components/TabGroup.tsx
+++ mojo/components/TabGroup.tsx
@@ -2,27 +2,31 @@ import { Pressable, Text, View } from 'react-native';
 import { cn } from '../lib/util';
 
 type TabGroupProps = {
-  selectedTab: string;
-  onSelect: (tab: string) => void;
+  selectedTab: 'send' | 'request';
+  onSelect: (tab: 'send' | 'request') => void;
   tabs: {
     label: string;
-    value: string;
+    value: 'send' | 'request';
   }[];
 };
 
 export const TabGroup = ({ selectedTab, onSelect, tabs }: TabGroupProps) => {
   return (
-    <View className="my-7 flex h-12 w-4/5 flex-row bg-[#3f3f3f] rounded-lg justify-center items-center gap-2 p-2">
+    <View className="my-7 flex h-12 w-4/5 flex-row items-center justify-center gap-2 rounded-lg bg-[#3f3f3f] p-2">
       {tabs.map((tab) => (
-        <View key={tab.value} className="flex-1 bg-[#212121] rounded-md justify-center items-center h-full overflow-hidden">
+        <View
+          key={tab.value}
+          className="h-full flex-1 items-center justify-center overflow-hidden rounded-md bg-[#212121]">
           <Pressable
             key={tab.value}
             onPress={() => onSelect(tab.value)}
             className={cn(
-              'flex flex-1 items-center justify-center h-full w-full',
+              'flex h-full w-full flex-1 items-center justify-center',
               selectedTab === tab.value && 'bg-background'
             )}>
-            <Text className="text-foreground w-full justify-center items-center text-center">{tab.label}</Text>
+            <Text className="text-foreground w-full items-center justify-center text-center">
+              {tab.label}
+            </Text>
           </Pressable>
         </View>
       ))}
