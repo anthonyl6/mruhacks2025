69985ec1dd0b464c5d74778fc4f2ad9f607686d3
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-10-05 10:49:57 -0600

    weewoo :D

--- mojo/components/SendAndRequestModal.tsx
+++ mojo/components/SendAndRequestModal.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { cn } from 'lib/util';
 import { X } from 'lucide-react-native';
+import { apiURL, useAuth } from 'providers/auth-provider';
 import { useState } from 'react';
 import { Pressable, Text, TextInput, View } from 'react-native';
 
@@ -13,10 +15,52 @@ export default function SendAndRequestModal({
   type: 'send' | 'request';
 }) {
   const [amount, setAmount] = useState(0);
+  const { authToken } = useAuth();
+
+  const handleSend = () => {
+    axios
+      .post(
+        `${apiURL}/payments/send`,
+        {
+          send_username: otherPartyDetails.email,
+          amount: amount,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        onClose();
+      });
+  };
+
+  const handleRequest = () => {
+    axios
+      .post(
+        `${apiURL}/payments/send`,
+        {
+          request_username: otherPartyDetails.email,
+          amount: amount,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        onClose();
+      });
+  };
+
   return (
     <View className="bg-background rel fixed inset-0 top-0 right-0 bottom-0 left-0 z-50 h-screen w-screen items-center justify-center">
       <View className="bg-background border-border relative mx-4 w-[90%] rounded-xl border p-8">
-        <Pressable className="absolute top-6 right-6 bg-gray-800 rounded-full p-2" onPress={onClose}>
+        <Pressable
+          className="absolute top-6 right-6 rounded-full bg-gray-800 p-2"
+          onPress={onClose}>
           <X size={20} color="#ffffff" />
         </Pressable>
         <Text className="text-foreground text-center text-xl font-medium">
@@ -38,6 +82,7 @@ export default function SendAndRequestModal({
         </View>
         <View className="flex flex-row items-center justify-center">
           <Pressable
+            onPress={type === 'send' ? handleSend : handleRequest}
             className={cn(
               'w-full rounded-full p-4',
               type === 'send' ? 'bg-sending-secondary' : 'bg-receiving-secondary'
